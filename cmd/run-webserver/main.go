ed90b54afb5cd71effecef389309853ff1999dce
Max Siegieda <maxsiegieda@gmail.com>
2017-11-09 16:10:23 +0000

    move FPGA setup to main

--- cmd/run-webserver/main.go
+++ cmd/run-webserver/main.go
@@ -2,17 +2,23 @@ package main
 
 import (
 	"crypto/md5"
-	//	"encoding/binary"
+	"encoding/binary"
 	"encoding/hex"
-	//	"log"
+	"log"
 	"net/http"
-	//	"xcl"
+	"xcl"
 
-	//	"github.com/ReconfigureIO/crypto/md5/host"
+	"github.com/ReconfigureIO/crypto/md5/host"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	//Set up FPGA
+	world := xcl.NewWorld()
+	defer world.Release()
+	program := world.Import("kernel_test")
+	defer program.Release()
+
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.String(200, "Visit /md5/{your_string_here} to have an FPGA hash that string")
@@ -20,8 +26,8 @@ func main() {
 	r.GET("/md5/:input", func(c *gin.Context) {
 		input := c.Param("input")
 		hash := GetMD5Hash(input)
-		//		fpgaHash := GetMD5HashFPGA(input)
-		c.String(http.StatusOK, "CPU says: %s, FPGA says: %s", hash, hash)
+		fpgaHash := GetMD5HashFPGA(world, program, input)
+		c.String(http.StatusOK, "CPU says: %s, FPGA says: %s", hash, fpgaHash)
 
 	})
 	r.Run(":80") // listen and serve on 0.0.0.0:80
@@ -34,36 +40,34 @@ func GetMD5Hash(text string) string {
 	return hex.EncodeToString(hasher.Sum(nil))
 }
 
-// func GetMD5HashFPGA(text string) string {
-// 	world := xcl.NewWorld()
-// 	defer world.Release()
+func GetMD5HashFPGA(world xcl.World, program xcl.Program, text string) string {
 
-// 	krnl := world.Import("kernel_test").GetKernel("reconfigure_io_sdaccel_builder_stub_0_1")
-// 	defer krnl.Release()
+	krnl := program.GetKernel("reconfigure_io_sdaccel_builder_stub_0_1")
+	defer krnl.Release()
 
-// 	msg := host.Pad([]byte(text))
-// 	msgSize := binary.Size(msg)
+	msg := host.Pad([]byte(text))
+	msgSize := binary.Size(msg)
 
-// 	inputBuff := world.Malloc(xcl.ReadOnly, uint(msgSize))
-// 	defer inputBuff.Free()
+	inputBuff := world.Malloc(xcl.ReadOnly, uint(msgSize))
+	defer inputBuff.Free()
 
-// 	outputBuff := world.Malloc(xcl.ReadOnly, 16)
-// 	defer outputBuff.Free()
+	outputBuff := world.Malloc(xcl.ReadOnly, 16)
+	defer outputBuff.Free()
 
-// 	binary.Write(inputBuff.Writer(), binary.LittleEndian, msg)
-// 	numBlocks := uint32(msgSize / 64)
+	binary.Write(inputBuff.Writer(), binary.LittleEndian, msg)
+	numBlocks := uint32(msgSize / 64)
 
-// 	krnl.SetArg(0, numBlocks)
-// 	krnl.SetMemoryArg(1, inputBuff)
-// 	krnl.SetMemoryArg(2, outputBuff)
+	krnl.SetArg(0, numBlocks)
+	krnl.SetMemoryArg(1, inputBuff)
+	krnl.SetMemoryArg(2, outputBuff)
 
-// 	krnl.Run(1, 1, 1)
+	krnl.Run(1, 1, 1)
 
-// 	ret := make([]byte, 16)
-// 	err := binary.Read(outputBuff.Reader(), binary.LittleEndian, ret)
-// 	if err != nil {
-// 		log.Fatal("binary.Read failed:", err)
-// 	}
+	ret := make([]byte, 16)
+	err := binary.Read(outputBuff.Reader(), binary.LittleEndian, ret)
+	if err != nil {
+		log.Fatal("binary.Read failed:", err)
+	}
 
-// 	return hex.EncodeToString(ret)
-// }
+	return hex.EncodeToString(ret)
+}
