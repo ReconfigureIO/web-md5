59e24b4558bd5530626feff03365cd32a062da43
Abiola Ibrahim <abiola89@gmail.com>
2017-11-16 14:28:11 +0100

    update template

--- cmd/run-webserver/main.go
+++ cmd/run-webserver/main.go
@@ -39,19 +39,58 @@ func GetMD5Hash(text string) string {
 }
 
 const webTemplate = `
+	<!DOCTYPE html>
 	<html>
 	<head>
-		<title>Web MD5 Example</title>
+		<title>MD5 Generator</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+		<meta charset="utf-8" />
+		<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans" />
+		<style>
+			body {
+				font-family: 'open sans', sans-serif;
+			}
+			#compute {
+				background-color: #37ca97;
+				color: #fff;
+				padding: 15px;
+				border: none;
+				font-size: 1.2em;
+				margin: 50px;
+				width: 200px;
+			}
+			textarea {
+				background-color: #eee;
+				width: 200px;
+				border: solid 1px #eee;
+				border-radius: 10px;
+				padding: 15px;
+				font-family: 'Open Sans', sans-serif;
+			}
+			#input {
+				min-height: 100px;
+			}
+			#output {
+				min-height: 200px;
+			}
+		</style>
 	</head>
 	<body>
-		<h3>Web MD5 Example</h3>
+		<div><img src="https://reconfigure.io/assets/dist/img/global/logo.svg" /></div>
+		<h2 style="color: #e9ab70;">MD5 Generator</h2>
 		<div>
-			Input:
-			<br />
-			<textarea id="input" name="input" style="border: solid 1px #555;"></textarea>
+			<p>Enter your text in the box on the left and click GENERATE to see the MD5 hash.</p>
+			<table>
+				<tbody>
+					<tr valign="top">
+					<td> <textarea id="input" name="input"></textarea> </td>
+					<td> <button id="compute" normal-label="GENERATE" busy-label="GENERATING..." >GENERATE</button> </td>
+					<td> <textarea id="output"></textarea> </td>
+					</tr>
+				</tbody>
+			</table>
 		</div>
-		<button id="compute">Compute MD5</button>
+
 		<div style="font-family: Monospace; display: none; padding-top: 10px;" id="resultDiv">
 			CPU: &nbsp;<span id="cpu"></span> <br />
 			FPGA: <span id="fpga"></span>
@@ -60,8 +99,8 @@ const webTemplate = `
 		<script>
 			function compute(){
 				$('#compute').attr('disabled', 'disabled');
-				$('#compute').text("Computing...");
-				$('#resultDiv').hide();
+				$('#compute').text($('#compute').attr('busy-label'));
+				$('#output').val("");
 
 				$.post('/md5', {input: $('#input').val()}, function(resp){
 					showResult(resp);
@@ -70,10 +109,9 @@ const webTemplate = `
 
 			function showResult(result){
 				$('#compute').removeAttr('disabled');
-				$('#compute').text("Compute MD5");
-				$('#cpu').html(result.cpu);
-				$('#fpga').html(result.fpga);
-				$('#resultDiv').show();
+				$('#compute').text($('#compute').attr('normal-label'));
+				$('#output').val(result.fpga);
+				$('#input').focus();
 			}
 
 			$('#compute').click(compute);
